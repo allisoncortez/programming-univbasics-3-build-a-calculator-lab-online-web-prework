55f908e27f637c8067dcb67252b34100c26d8d8e
allison cortez <allizetorc@gmail.com>
2019-09-28 00:58:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,25 @@
-def addition(num1, num2)
+def addition (num1, num2)
   num1 + num2
 end
 
-def subtraction(num1, num2)
+def subtraction (num1, num2)
   num1 - num2
 end
 
-def division(num1, num2)
+def division (num1, num2)
   num1 / num2
 end
 
-def multiplication(num1, num2)
+def multiplication (num1, num2)
   num1 * num2
 end
 
-def modulo(num1, num2)
-  num1 * num2
-  puts modulo
+def modulo (num1, num2)
+  modulo = num1 % num2
+  
 end
 
 def square_root(num)
-  num **
-end
+  return Math.sqrt(num)
+
+  end
